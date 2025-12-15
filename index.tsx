6a7502e53ac5a9684cd0a0b177a3ec398adea39b
kenhgt247 <34070504+kenhgt247@users.noreply.github.com>
2025-12-15 10:42:49 +0700

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -6,7 +6,7 @@ import App from './App';
 // Register Service Worker for PWA
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./service-worker.js')
+    navigator.serviceWorker.register('/service-worker.js')
       .then(registration => {
         console.log('SW registered: ', registration);
       })
