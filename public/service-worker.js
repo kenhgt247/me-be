f9c7c5a9a1a01ecc7038a1f47c528629e557ed83
kenhgt247 <34070504+kenhgt247@users.noreply.github.com>
2025-12-16 16:22:51 +0700

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,47 @@
 
+const CACHE_NAME = 'asking-vn-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  // Simple cache-first strategy for static assets, network-first for others could be implemented here.
+  // For PWA install criteria, a simple fetch handler is sufficient.
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
